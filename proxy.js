fbf69f5c883f8b9f946d780601d8ad14ecfe3e5e
pallet0 <106745874+pallet0@users.noreply.github.com>
2025-11-04 15:55:35 +0900

    Refactor proxy server to include CORS support

--- proxy.js
+++ proxy.js
@@ -1,32 +1,22 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
-
-// 냥! 💖 여기가 제일 중요해요! 주인님!
-// Kider 파일에 있던 주소 대신에, index.html에 있던
-// 주인님의 Apps Script 주소로 바꿔야 해요!
 const GOOGLE_SCRIPT_PATH = '/macros/s/AKfycbzpv5ESZVnEzVkQTSJoSAyag5e2KPdxvWYNuNyliFu8eoI7FV90dlqjpr_0PaCv4pKLNw/exec';
 
 app.use('/api', createProxyMiddleware({
     target: 'https://script.google.com',
     changeOrigin: true,
     pathRewrite: {
-        // 🚨 이 부분이...
-        // '^/api': '/macros/s/AKfycbymucHyMeANwDRi7xtl0IbXppo4PJt8DgWmsAK4g-KMBKuZ6veqCZymTy2GpVqPVLX5/exec'
-        // ✨ 이렇게 바뀌어야 해요! 냥!
         '^/api': GOOGLE_SCRIPT_PATH
     },
     onProxyReq: (proxyReq, req, res) => {
-        // 원본 URL의 쿼리 문자열을 유지
         const originalUrl = new URL(req.url, `http://${req.headers.host}`);
-        
-        // 🚨 이 부분도...
-        // proxyReq.path = '/macros/s/AKfycbymucHyMeANwDRi7xtl0IbXppo4PJt8DgWmsAK4g-KMBKuZ6veqCZymTy2GpVqPVLX5/exec' + originalUrl.search;
-        // ✨ 이렇게 바뀌어야 해요! 냥!
         proxyReq.path = GOOGLE_SCRIPT_PATH + originalUrl.search;
-        
+    
         console.log('Proxying request to:', proxyReq.path);
     },
     onProxyRes: (proxyRes, req, res) => {
@@ -41,4 +31,5 @@ app.use('/api', createProxyMiddleware({
 
 app.listen(PORT, () => {
     console.log(`Proxy server running on port ${PORT}`);
+
 });
